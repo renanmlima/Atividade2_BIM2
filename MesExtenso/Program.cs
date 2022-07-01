26d27598119bda84f51dc0862d47a2582e73d1fb
Renan Lima <105513340+renanmlima@users.noreply.github.com>
2022-07-01 13:36:06 -0300

    Update Program.cs

--- MesExtenso/Program.cs
+++ MesExtenso/Program.cs
@@ -8,24 +8,31 @@ namespace MesExtenso
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static string mesConverte(DateTime atual)
         {
-            Console.Write("Por favor, insira um data no formato DD/MM/AAAA. Nós te apresentaremos o mês escrito em extenso: ");
-            //variavel responsavel por armazenar a data atual inserida pelo usuário
-            var data_atual = Convert.ToDateTime(Console.ReadLine());
 
             //variavel que irá armazenar o número do mês, utilizando a função .Month
-            int mes = data_atual.Month;
+            int mes = atual.Month;
 
             //array que armazena os meses em extenso
             string[] mes_extenso = new string[12] { "Janeiro", "Fevereiro", "Março",
             "Abril", "Maio", "Junho", "Julho","Agosto","Setembro","Outubro","Novembro", "Dezembro"};
 
-            //aqui será mostrado o mês em extenso, onde pegamos o valor da variavel mês e subtraimos por 1, para localizar a posição no array.
-            Console.WriteLine(mes_extenso[mes - 1]);
+            //aqui será mostrado o mês em extenso, onde pegamos o
+            //valor da variavel mês e subtraimos por 1, para localizar a posição no array.
+            return (mes_extenso[mes - 1]);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Por favor, insira um data no formato DD/MM/AAAA. Nós te apresentaremos o mês escrito em extenso: ");
+            //variavel responsavel por armazenar a data atual inserida pelo usuário
+            
+            var data_atual = Convert.ToDateTime(Console.ReadLine());
+
+            mesConverte(data_atual);
 
             Console.ReadKey();
-
         }
     }
 }
